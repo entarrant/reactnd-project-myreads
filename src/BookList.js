0e588c0d145f2210518be61558e746cf0f4c6f67
Emily Tarrant <emily.tarrant@bigcommerce.com>
2019-04-01 11:26:12 -0500

    Add basic BookList, Book, and ShelfChanger components
    The ShelfChanger currently doesn't make use of the BooksAPI and all
    of the lists just get the same unfiltered list of initial books.

---
+++ src/BookList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Book from "./Book";
+
+class BookList extends React.Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map(book => (
+              <Book key={book.id} book={book} />
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookList;
